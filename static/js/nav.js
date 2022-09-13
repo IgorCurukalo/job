02769af0f85ebdeee862482c71fbb279ea1f7851
IgorCurukalo <IgorCurukalo@gmail.com>
2022-09-13 11:28:08 +0500

    html-css-js-update

---
+++ static/js/nav.js
@@ -0,0 +1,15 @@
+function openNavMenu() {
+    document.getElementById("myNavMenu").style.width = "100%";
+}
+
+function closeNavMenu() {
+    document.getElementById("myNavMenu").style.width = "0%";
+}
+
+function openNavLinks() {
+    document.getElementById("myNavLinks").style.width = "100%";
+}
+
+function closeNavLinks() {
+    document.getElementById("myNavLinks").style.width = "0%";
+}
